90bdde6a4c30d8944388f66fb388c25b7f1f8b7b
reotak <reotak@gmail.com>
2015-07-01 21:22:58 +0900

    2:first

--- hello.c
+++ hello.c
@@ -4,7 +4,13 @@ char *hello() {
     return "hello, world";
 }
 
+char *hello2() {
+    return "\nhello2\n";
+}
+
 int main(void) {
     printf(hello());
+    printf(hello2());
+    
     return 0;
 }
